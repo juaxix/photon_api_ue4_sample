16a0bd9aef22a5c1b2abf573d40ead034769ed29
juaxix <juaxix@gmail.com>
2020-01-29 22:20:22 +0100

    Organizing directories

--- Source/RollingPhotonCloud/RollingPhotonCloud.h
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2019 jbelon - juaxix - xixgames, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
---
+++ UE4_22/Source/RollingPhotonCloud/RollingPhotonCloud.h
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 jbelon - juaxix - xixgames, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
