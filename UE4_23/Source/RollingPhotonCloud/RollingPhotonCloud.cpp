923d3d431203648fbb74787328e1a1be3e903d16
juaxix <juaxix@gmail.com>
2020-01-29 22:25:08 +0100

    Added project for Unreal 4.23

---
+++ UE4_23/Source/RollingPhotonCloud/RollingPhotonCloud.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 jbelon - juaxix - xixgames, Inc. All Rights Reserved.
+
+#include "RollingPhotonCloud.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RollingPhotonCloud, "RollingPhotonCloud" );
