3569575dac2b6eaf577207d4041a99d3c5c6d2db
Juan Belón <juaxix@gmail.com>
2020-12-05 10:00:36 +0000

    Added 4.26 , fixed some blueprints, removing ids from apps

---
+++ UE4_26/Source/RollingPhotonCloud.Target.cs
@@ -0,0 +1,17 @@
+// Copyright 1998-2020 jbelon - juaxix - xixgames, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class RollingPhotonCloudTarget : TargetRules
+{
+	public RollingPhotonCloudTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		bLegacyPublicIncludePaths = false;
+		ShadowVariableWarningLevel = WarningLevel.Error;
+
+		ExtraModuleNames.AddRange(new string[] {"RollingPhotonCloud"});
+	}
+}
