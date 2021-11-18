1f3b2854043ae55f577a1d7ef02f840d5067b79b
Juan Belon <juaxix@gmail.com>
2021-11-18 10:54:03 +0000

    Photon Voice project added

---
+++ PhotonVoiceIntegration/Source/RollingPhotonCloud/Private/RollingGameModeBase.cpp
@@ -0,0 +1,25 @@
+// Copyright 1998-2020 jbelon - juaxix - xixgames, Inc. All Rights Reserved.
+#include "RollingGameModeBase.h"
+#include "PhotonCloudAPIBPLibrary.h"
+#include "PhotonCloudSubsystem.h"
+
+ARollingGameModeBase::ARollingGameModeBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	bUseSeamlessTravel = true;
+}
+
+void ARollingGameModeBase::GetSeamlessTravelActorList(bool bToTransition, TArray<AActor*>& ActorList)
+{
+	// add actors for seamless travel, for example from the list of observed actors
+	if (UPhotonCloudObject* PhotonCloud = GetGameInstance()->GetSubsystem<UPhotonCloudSubsystem>()->GetPhotonCloudAPI())
+	{
+		const auto& ObservedMechanicsActors = PhotonCloud->GetObservedMechanicsActors();
+		for (auto& ObservedMechanicsActor: ObservedMechanicsActors)
+		{
+			if (IsValid(ObservedMechanicsActor.Value) && ObservedMechanicsActor.Value->IsA(AActor::StaticClass()))
+			{
+				ActorList.Add(Cast<AActor>(ObservedMechanicsActor.Value));
+			}
+		}
+	}
+}
