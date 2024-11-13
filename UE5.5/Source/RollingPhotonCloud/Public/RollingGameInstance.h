19fd4d6e72e9ea8673dd7667a15d734a127dfdc2
Juan Belon <juaxix@gmail.com>
2024-11-13 09:18:32 +0100

    Adding UE 5.5 rolling sample project

---
+++ UE5.5/Source/RollingPhotonCloud/Public/RollingGameInstance.h
@@ -0,0 +1,14 @@
+// Copyright 1998-2024 Juan Belón - xixgames -  Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RollingGameInstance.generated.h"
+
+UCLASS()
+class ROLLINGPHOTONCLOUD_API URollingGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+};
